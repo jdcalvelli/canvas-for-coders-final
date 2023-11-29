ec8b2753c22b460c2b784270072246934f174714
JD Calvelli <jdcalvelli@JDs-MacBook-Pro.local>
2023-11-28 23:53:31 -0500

    fix import issue

--- src/__components/_sounds/soundObj.ts
+++ src/__components/_sounds/soundObj.ts
@@ -1,5 +1,5 @@
 import * as THREE from "three";
-import { audioGlobals } from "../__globals/audioGlobals";
+import { audioGlobals } from "../../__globals/audioGlobals";
 
 export function createSound(
   name: string,
